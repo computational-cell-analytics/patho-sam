0662de11f5413fb779e8932f3afed425035add43
titusgriebel <titus.griebel@gmx.de>
2025-01-26 18:07:28 +0100

    fixed path imports

--- experiments/data/get_paths.py
+++ experiments/data/get_paths.py
@@ -6,8 +6,9 @@ import os
 from natsort import natsorted
 
 from torch_em.data.datasets.histopathology import (
-    consep, cpm, cryonuseg, glas, lizard, lynsec, monuseg, nuclick,
-    nuinsseg, pannuke, puma, srsanet, tnbc)
+    consep, cpm, cryonuseg, lizard, lynsec, monuseg, nuclick,
+    nuinsseg, pannuke, puma, srsanet, tnbc
+)
 
 
 def get_dataset_paths(data_path, dataset) -> list:
