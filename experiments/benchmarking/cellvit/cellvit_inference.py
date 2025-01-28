3c87e8f37605070dc96277ee0dc893ff471cc4b9
anwai98 <anwai.archit@gmail.com>
2025-01-29 00:20:22 +0100

    Minor updates to code semantics

--- experiments/benchmarking/cellvit/cellvit_inference.py
+++ experiments/benchmarking/cellvit/cellvit_inference.py
@@ -12,39 +12,40 @@ CHECKPOINTS = ["256-x20", "256-x40", "SAM-H-x20", "SAM-H-x40"]
 def run_inference(model_dir, input_dir, output_dir, result_dir, datasets=None, checkpoints=None):
     if datasets is None:
         datasets = DATASETS
+
     if checkpoints is None:
         checkpoints = CHECKPOINTS
+
     for dataset in datasets:
         if dataset not in ["pannuke", "nuclick", "srsanet", "lizard", "cpm15", "consep", "cpm17"]:
             data_dir = os.path.join(input_dir, "original_data", dataset, "eval_split")
         else:
             data_dir = os.path.join(input_dir, "vit_data", dataset, "eval_split")
 
-        for checkpoint in ["SAM-H-x40"]:  # ["256-x20", "256-x40", "SAM-H-x20", "SAM-H-x40"]:
+        for checkpoint in checkpoints:
             model_path = os.path.join(model_dir, f"CellViT-{checkpoint}.pth")
-            if os.path.exists(os.path.join(result_dir, dataset, checkpoint, f'{dataset}_cellvit_{checkpoint}_ais_result.csv')):
+            if os.path.exists(
+                os.path.join(result_dir, dataset, checkpoint, f'{dataset}_cellvit_{checkpoint}_ais_result.csv')
+            ):
                 print(f"Inference with CellViT model (type: {checkpoint}) on {dataset} dataset already done")
                 continue
 
             output_path = os.path.join(output_dir, dataset, checkpoint)
             os.makedirs(output_path, exist_ok=True)
             args = [
-                "--model",
-                f"{model_path}",
-                "--outdir",
-                f"{output_path}",
-                "--magnification",
-                "40",
-                "--data",
-                f"{data_dir}",
+                "--model", f"{model_path}",
+                "--outdir", f"{output_path}",
+                "--magnification", "40",
+                "--data", f"{data_dir}",
             ]
             if dataset == "lizard":
                 args.append("--stitched")
 
             command = [
-                "python3",
-                "/user/titus.griebel/u12649/CellViT/cell_segmentation/inference/inference_cellvit_experiment_monuseg.py",  # noqa
+                "python",
+                os.path.expanduser("~/CellViT/cell_segmentation/inference/inference_cellvit_experiment_monuseg.py"),
             ] + args
+
             print(f"Running inference with CellViT {checkpoint} model on {dataset} dataset...")
             subprocess.run(command)
 
@@ -67,6 +68,7 @@ def run_inference(model_dir, input_dir, output_dir, result_dir, datasets=None, c
 #         "/mnt/lustre-grete/usr/u12649/models/cellvit/results",
 #     )
 
+
 def get_cellvit_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-m", "--model_dir", type=str, default=None, help="The path to the checkpoints")
@@ -84,6 +86,7 @@ def main():
         model_dir=args.model_dir,
         input_dir=args.input,
         output_dir=args.output_path,
+        result_dir=args.output_path,
         datasets=[args.dataset],
         checkpoints=[args.checkpoint],
     )
