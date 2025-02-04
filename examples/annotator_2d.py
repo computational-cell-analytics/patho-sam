75c01dd7fbbfa6653a4c1651ea08ccf4e8d7535c
Anwai Archit <anwai.archit@gmail.com>
2025-02-04 15:34:39 +0100

    Add running scripts for WSI

--- examples/annotator_2d.py
+++ examples/annotator_2d.py
@@ -4,23 +4,32 @@ from tukra.io import read_image
 
 from micro_sam.sam_annotator import annotator_2d
 
-from medico_sam.util import get_cache_directory
-from medico_sam.sample_data import fetch_dermoscopy_example_data
+
+# DATA_CACHE = os.path.join(get_cache_directory(), "sample_data")
 
 
-DATA_CACHE = os.path.join(get_cache_directory(), "sample_data")
-
-
-def uwaterloo_skin_annotator(use_finetuned_model):
+def whole_slide_image_annotator(use_finetuned_model):
     """Run the 2d annotator for an example image from the UWaterloo Skin dataset.
 
     See https://uwaterloo.ca/vision-image-processing-lab/research-demos/skin-cancer-detection for details on the data.
     """
-    example_data = fetch_dermoscopy_example_data(DATA_CACHE)
-    image = read_image(example_data)
+    # example_data = fetch_dermoscopy_example_data(DATA_CACHE)
+
+    fpath = "./CMU-1.svs"
+
+    image = read_image(fpath, image_size=(10000, 10000, 10000, 10000), scale=None)
+
+    print(image.shape)
+
+    import napari
+    v = napari.Viewer()
+    v.add_image(image)
+    napari.run()
+
+    breakpoint()
 
     if use_finetuned_model:
-        model_type = "vit_b_medical_imaging"
+        model_type = "vit_b_histopathology"
     else:
         model_type = "vit_b"
 
@@ -32,7 +41,7 @@ def main():
     use_finetuned_model = True
 
     # 2d annotator for uwaterloo-skin data
-    uwaterloo_skin_annotator(use_finetuned_model)
+    whole_slide_image_annotator(use_finetuned_model)
 
 
 if __name__ == "__main__":
