f7d4a00bb5ef6c36f10fc484892406fc316fdc81
Anwai Archit <anwai.archit@gmail.com>
2025-02-04 18:09:26 +0100

    Finalize whole-slide image for embeddings

--- examples/annotator_2d.py
+++ examples/annotator_2d.py
@@ -2,45 +2,35 @@ import os
 
 from tukra.io import read_image
 
+from micro_sam.util import get_cache_directory
 from micro_sam.sam_annotator import annotator_2d
+from micro_sam.sample_data import fetch_wholeslide_histopathology_example_data
 
 
-# DATA_CACHE = os.path.join(get_cache_directory(), "sample_data")
+DATA_CACHE = os.path.join(get_cache_directory(), "sample_data")
 
 
 def whole_slide_image_annotator(use_finetuned_model):
-    """Run the 2d annotator for an example image from the UWaterloo Skin dataset.
+    """Run the 2d annotator for an example whole-slide image for histopathology.
 
-    See https://uwaterloo.ca/vision-image-processing-lab/research-demos/skin-cancer-detection for details on the data.
+    See 'fetch_wholeslide_histopathology_example_data' for details on the data.
     """
-    # example_data = fetch_dermoscopy_example_data(DATA_CACHE)
-
-    fpath = "./CMU-1.svs"
-
-    image = read_image(fpath, image_size=(10000, 10000, 10000, 10000), scale=None)
-
-    print(image.shape)
-
-    import napari
-    v = napari.Viewer()
-    v.add_image(image)
-    napari.run()
-
-    breakpoint()
+    example_data = fetch_wholeslide_histopathology_example_data(DATA_CACHE)
+    image = read_image(example_data, image_size=(10000, 10000, 10000, 10000), scale=None)
 
     if use_finetuned_model:
         model_type = "vit_b_histopathology"
     else:
         model_type = "vit_b"
 
-    annotator_2d(image=image, model_type=model_type)
+    annotator_2d(image=image, embedding_path=f"./embedding_{model_type}.zarr", model_type=model_type)
 
 
 def main():
     # Whether to use the fine-tuned SAM model for WSIs.
     use_finetuned_model = True
 
-    # 2d annotator for uwaterloo-skin data
+    # 2d annotator for WSI data.
     whole_slide_image_annotator(use_finetuned_model)
 
 
