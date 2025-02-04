90f7a92e045b6d782f9131e8b0a4ae17aa28ade2
titusgriebel <titus.griebel@gmx.de>
2025-02-04 14:22:46 +0100

    added specialist finetuning example

---
+++ examples/finetune_specialist.py
@@ -0,0 +1,132 @@
+import os
+import argparse
+import torch
+
+from util import get_dataloaders
+
+import micro_sam.training as sam_training
+from micro_sam.util import export_custom_sam_model
+
+
+def get_dataloader(patch_shape, batch_size, dataset, data_dir, images_dir, labels_dir):
+    """Return train or val data loader for finetuning SAM.
+
+    The data loader must be a torch data loader that retuns `x, y` tensors,
+    where `x` is the image data and `y` are the labels.
+    The labels have to be in a label mask instance segmentation format.
+    I.e. a tensor of the same spatial shape as `x`, with each object mask having its own ID.
+    Important: the ID 0 is reserved for background, and the IDs must be consecutive
+
+    Here, we use `torch_em.default_segmentation_loader` for creating a suitable data loader from
+    the example dataset data or from provided custom images and labels. You can either adapt this 
+    for your own data (see comments below) or write a suitable torch dataloader yourself.
+    """
+    if dataset is not None:
+        os.makedirs(data_dir, exist_ok=True)
+        return get_dataloaders(patch_shape, batch_size, data_path=data_dir, dataset=dataset)
+
+    else:
+        return get_dataloaders(patch_shape, batch_size, images_dir=images_dir, labels_dir=labels_dir)
+
+    # This will download the image and segmentation data for training if a dataset has been chosen. Otherwise,
+    # the provided images and masks directories will be used to build the torch dataloader.
+
+    # torch_em.default_segmentation_loader is a convenience function to build a torch dataloader
+    # from image data and labels for training segmentation models.
+    # It supports image data in various formats. Here, we either load the dataset data via torch-em or load custom
+    # training data from specified images and labels directories.
+
+
+def run_training(checkpoint_name, model_type, dataset, data_dir, images_dir, labels_dir, save_root):
+    """Run the actual model training."""
+
+    # All hyperparameters for training.
+    batch_size = 1  # the training batch size
+    patch_shape = (512, 512)  # the size of patches for training
+    n_iterations = 10e4  # the number of iterations to train for
+    n_objects_per_batch = 25  # the number of objects per batch that will be sampled
+    device = "cuda" if torch.cuda.is_available() else "cpu"  # the device/GPU used for training
+
+    # Get the dataloaders.
+    train_loader, val_loader = get_dataloader(patch_shape, batch_size, dataset, data_dir, images_dir, labels_dir)
+
+    # Run training.
+    sam_training.train_sam(
+        name=checkpoint_name,
+        model_type=model_type,
+        train_loader=train_loader,
+        val_loader=val_loader,
+        n_iterations=n_iterations,
+        n_objects_per_batch=n_objects_per_batch,
+        device=device,
+        save_root=save_root
+    )
+    export_model(checkpoint_name=checkpoint_name, model_type=model_type)
+
+
+def export_model(checkpoint_name, model_type):
+    """Export the trained model."""
+    # export the model after training so that it can be used by the rest of the micro_sam library
+    export_path = "./finetuned_specialist_model.pth"
+    checkpoint_path = os.path.join("checkpoints", checkpoint_name, "best.pt")
+    export_custom_sam_model(
+        checkpoint_path=checkpoint_path,
+        model_type=model_type,
+        save_path=export_path,
+    )
+
+
+def finetune_specialist(args):
+    """Example code for finetuning SAM on histopathology datasets."""
+    # override this (below) if you have some more complex set-up and need to specify the exact GPU.
+
+    # training settings:
+    model_type = args.model_type
+    checkpoint_name = "sam_specialist"
+    run_training(checkpoint_name=checkpoint_name, model_type=model_type, dataset=args.dataset, data_dir=args.data_dir,
+                 images_dir=args.images_dir, labels_dir=args.labels_dir, save_root=args.save_root)
+
+
+def main():
+    """Finetune a Segment Anything model.
+
+    This example can easily be adapted for other data (including data you have annoated with micro_sam beforehand).
+
+    Option 1: Provide the name of a dataset implemented in torch-em for finetuning. Leave images_dir and labels_dir
+    blank but provide a data path where the dataset will be downloaded to.
+
+    Option 2: Provide images_dir and labels_dir to train from custom data. Images must be in shape (H, W, 3), labels in
+    (H, W).
+    """
+
+    parser = argparse.ArgumentParser(description="Finetune Segment Anything for the Histopathology datasets.")
+    parser.add_argument(
+        "--data_path", default=None,
+        help="(Optional) path where dataset will be loaded if dataset argument is provided.",
+    )
+    parser.add_argument(
+        "--dataset", "-d", default=None,
+        help="(Optional) Which dataset to finetune the specialist model on. Choose from datasets implemented in torch-em.",
+    )
+    parser.add_argument(
+        "--images_dir", default=None,
+        help="(Optional) Path to training images.",
+    )
+    parser.add_argument(
+        "--labels_dir", default=None,
+        help="(Optional) Path to labels corresponding to training images in images_dir.",
+    )
+    parser.add_argument(
+        "--model_type", "-m", default="vit_b",
+        help="The model type to use for fine-tuning. Either vit_t, vit_b, vit_l or vit_h.",
+    )
+    parser.add_argument(
+        "--save_root", "-s", default=None,
+        help="Where to save the checkpoint and logs. By default they will be saved where this script is run.",
+    )
+    args = parser.parse_args()
+    finetune_specialist(args)
+
+
+if __name__ == "__main__":
+    main()
---
+++ examples/util.py
@@ -0,0 +1,337 @@
+import os
+from glob import glob
+from natsort import natsorted
+
+
+import torch
+import torch.utils.data as data_util
+import torch_em
+from torch_em.data import MinInstanceSampler, datasets
+from torch_em.transform.label import PerObjectDistanceTransform
+from torch_em.data.datasets.light_microscopy.neurips_cell_seg import to_rgb
+
+
+def histopathology_identity(x, ensure_rgb=True):
+    """Identity transform.
+    Inspired from 'micro_sam/training/util.py' -> 'identity' function.
+
+    This ensures to skip data normalization when finetuning SAM.
+    Data normalization is performed within the model to SA-1B data statistics
+    and should thus be skipped as a preprocessing step in training.
+    """
+    if ensure_rgb:
+        x = to_rgb(x)
+
+    return x
+
+
+def _get_train_val_split(ds, val_fraction=0.2):
+    generator = torch.Generator().manual_seed(42)
+    train_ds, val_ds = data_util.random_split(ds, [1 - val_fraction, val_fraction], generator=generator)
+    return train_ds, val_ds
+
+
+def get_dataloaders(patch_shape, batch_size, data_path, dataset, images_dir, masks_dir):
+    label_dtype = torch.float32
+    sampler = MinInstanceSampler(min_num_instances=4, min_size=10)
+
+    # Expected raw and label transforms.
+    raw_transform = histopathology_identity
+    label_transform = PerObjectDistanceTransform(
+        distances=True,
+        boundary_distances=True,
+        directed_distances=False,
+        foreground=True,
+        instances=True,
+        min_size=10,
+    )
+
+    if dataset == "consep":
+        ds = datasets.get_consep_dataset(
+            path=data_path,
+            patch_shape=patch_shape,
+            download=True,
+            split="train",
+            sampler=sampler,
+            label_dtype=label_dtype,
+            label_transform=label_transform,
+            raw_transform=raw_transform,
+        )
+
+    elif dataset == "cpm15":
+        train_ds = datasets.get_cpm_dataset(
+            path=data_path,
+            patch_shape=patch_shape,
+            download=False,
+            split="train",
+            data_choice="cpm15",
+            sampler=sampler,
+            label_dtype=label_dtype,
+            label_transform=label_transform,
+            raw_transform=raw_transform,
+        )
+        val_ds = datasets.get_cpm_dataset(
+            path=data_path,
+            patch_shape=patch_shape,
+            download=False,
+            split="val",
+            data_choice="cpm15",
+            sampler=sampler,
+            label_dtype=label_dtype,
+            label_transform=label_transform,
+            raw_transform=raw_transform,
+        )
+
+    elif dataset == "cpm17":
+        ds = datasets.get_cpm_dataset(
+            path=data_path,
+            patch_shape=patch_shape,
+            download=False,
+            split="train",
+            data_choice="cpm17",
+            sampler=sampler,
+            label_dtype=label_dtype,
+            label_transform=label_transform,
+            raw_transform=raw_transform,
+        )
+
+    elif dataset == "cryonuseg":
+        train_loader = datasets.get_cryonuseg_loader(
+            path=data_path,
+            patch_shape=(1,) + patch_shape,
+            rater="b1",
+            split="train",
+            download=True,
+            sampler=sampler,
+            label_dtype=label_dtype,
+            label_transform=label_transform,
+            raw_transform=raw_transform,
+        )
+        val_loader = datasets.get_cryonuseg_loader(
+            path=data_path,
+            patch_shape=(1,) + patch_shape,
+            rater="b1",
+            split="val",
+            download=True,
+            sampler=sampler,
+            label_dtype=label_dtype,
+            label_transform=label_transform,
+            raw_transform=raw_transform,
+        )
+
+    elif dataset == "glas":
+        ds = datasets.get_glas_dataset(
+            path=data_path,
+            patch_shape=patch_shape,
+            download=True,
+            split="train",
+            label_dtype=label_dtype,
+            label_transform=label_transform,
+            raw_transform=raw_transform,
+            sampler=MinInstanceSampler(min_num_instances=2),
+        )
+
+    elif dataset == "lizard":
+        train_ds = datasets.get_lizard_dataset(
+            path=data_path,
+            patch_shape=patch_shape,
+            download=True,
+            split="train",
+            sampler=sampler,
+            label_dtype=label_dtype,
+            label_transform=label_transform,
+            raw_transform=raw_transform,
+        )
+        val_ds = datasets.get_lizard_dataset(
+            path=data_path,
+            patch_shape=patch_shape,
+            download=True,
+            split="val",
+            sampler=sampler,
+            label_dtype=label_dtype,
+            label_transform=label_transform,
+            raw_transform=raw_transform,
+        )
+    # this will not have an exclusive test split
+
+    elif dataset == "lynsec_he":
+        ds = datasets.get_lynsec_dataset(
+            path=data_path,
+            patch_shape=patch_shape,
+            choice="h&e",
+            download=True,
+            sampler=sampler,
+            label_dtype=label_dtype,
+            label_transform=label_transform,
+            raw_transform=raw_transform,
+        )
+    # this will not have an exclusive test split
+    elif dataset == "lynsec_ihc":
+        ds = datasets.get_lynsec_dataset(
+            path=data_path,
+            patch_shape=patch_shape,
+            choice="ihc",
+            download=True,
+            sampler=sampler,
+            label_dtype=label_dtype,
+            label_transform=label_transform,
+            raw_transform=raw_transform,
+        )
+
+    elif dataset == "monusac":
+        ds = datasets.get_monusac_dataset(
+            path=data_path,
+            patch_shape=patch_shape,
+            split="train",
+            download=True,
+            sampler=sampler,
+            label_dtype=label_dtype,
+            label_transform=label_transform,
+            raw_transform=raw_transform,
+        )
+
+    elif dataset == "monuseg":
+        ds = datasets.get_monuseg_dataset(
+            path=data_path,
+            patch_shape=patch_shape,
+            split="train",
+            download=True,
+            sampler=sampler,
+            label_dtype=label_dtype,
+            label_transform=label_transform,
+            raw_transform=raw_transform,
+        )
+    # this will not have an exclusive test split
+    elif dataset == "nuinsseg":
+        ds = datasets.get_nuinsseg_dataset(
+            path=data_path,
+            patch_shape=patch_shape,
+            download=True,
+            sampler=sampler,
+            label_dtype=label_dtype,
+            label_transform=label_transform,
+            raw_transform=raw_transform,
+        )
+
+    elif dataset == "nuclick":
+        ds = datasets.get_nuclick_dataset(
+            path=data_path,
+            patch_shape=patch_shape,
+            download=True,
+            split="Train",
+            sampler=MinInstanceSampler(min_num_instances=2, min_size=10),
+            label_dtype=label_dtype,
+            label_transform=label_transform,
+            raw_transform=raw_transform,
+        )
+
+    elif dataset == "pannuke":
+        ds = datasets.get_pannuke_dataset(
+            path=data_path,
+            patch_shape=(1,) + patch_shape,
+            folds=["fold_1", "fold_2"],
+            ndim=2,
+            download=True,
+            sampler=sampler,
+            label_dtype=label_dtype,
+            label_transform=label_transform,
+            raw_transform=raw_transform,
+        )
+
+    elif dataset == "puma":
+        train_ds = datasets.get_puma_dataset(
+            path=data_path,
+            patch_shape=patch_shape,
+            annotations="nuclei",
+            download=True,
+            split="train",
+            sampler=sampler,
+            label_dtype=label_dtype,
+            label_transform=label_transform,
+            raw_transform=raw_transform,
+        )
+        val_ds = datasets.get_puma_dataset(
+            path=data_path,
+            patch_shape=patch_shape,
+            batch_size=1,
+            annotations="nuclei",
+            download=True,
+            split="val",
+            sampler=sampler,
+            label_dtype=label_dtype,
+            label_transform=label_transform,
+            raw_transform=raw_transform,
+        )
+
+    elif dataset == "srsanet":
+        train_ds = datasets.get_srsanet_dataset(
+            path=data_path,
+            patch_shape=patch_shape,
+            download=True,
+            split="train",
+            sampler=sampler,
+            label_dtype=label_dtype,
+            label_transform=label_transform,
+            raw_transform=raw_transform,
+        )
+        val_ds = datasets.get_srsanet_dataset(
+            path=data_path,
+            patch_shape=patch_shape,
+            download=True,
+            split="val",
+            sampler=sampler,
+            label_dtype=label_dtype,
+            label_transform=label_transform,
+            raw_transform=raw_transform,
+        )
+
+    elif dataset == "tnbc":
+        train_ds = datasets.get_tnbc_dataset(
+            path=data_path,
+            patch_shape=patch_shape,
+            ndim=2,
+            download=True,
+            split="train",
+            sampler=sampler,
+            label_dtype=label_dtype,
+            label_transform=label_transform,
+            raw_transform=raw_transform,
+        )
+        val_ds = datasets.get_tnbc_dataset(
+            path=data_path,
+            patch_shape=patch_shape,
+            ndim=2,
+            download=True,
+            split="val",
+            sampler=sampler,
+            label_dtype=label_dtype,
+            label_transform=label_transform,
+            raw_transform=raw_transform,
+        )
+    elif dataset is None:
+        raw_paths = natsorted(glob(os.path.join(images_dir, "*")))
+        label_paths = glob(os.path.join(masks_dir, "*"))
+
+        assert len(raw_paths) == len(label_paths)
+
+        ds = torch_em.default_segmentation_dataset(
+            raw_paths=raw_paths,
+            raw_key=None,
+            label_paths=label_paths,
+            label_key=None,
+            patch_shape=patch_shape,
+            ndim=2,
+            with_channels=True,
+            sampler=sampler,
+            label_dtype=label_dtype,
+            label_transform=label_transform,
+            raw_transform=raw_transform,
+            )
+
+    if ds is not None:
+        train_ds, val_ds = _get_train_val_split(ds=ds)
+
+    train_loader = torch_em.get_data_loader(train_ds, batch_size=batch_size, shuffle=True, num_workers=16)
+    val_loader = torch_em.get_data_loader(val_ds, batch_size=1, shuffle=True, num_workers=16)
+
+    return train_loader, val_loader
