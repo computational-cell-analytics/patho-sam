b5fa7798988e191e5e325b9891f5a293ac43d5d5
anwai98 <anwai.archit@gmail.com>
2025-10-08 17:14:24 +0200

    Update installation from source to add find_packages

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import runpy
-from distutils.core import setup
+from setuptools import find_packages, setup
 
 
 __version__ = runpy.run_path("patho_sam/__version__.py")["__version__"]
@@ -14,12 +14,12 @@ setup(
     author='Titus Griebel, Anwai Archit, Constantin Pape',
     author_email='titus.griebel@stud.uni-goettingen.de, anwai.archit@uni-goettingen.de, constantin.pape@informatik.uni-goettingen.de',  # noqa
     url='https://github.com/computational-cell-analytics/patho-sam',
-    packages=['patho_sam'],
+    packages=find_packages(include=['patho_sam', 'patho_sam.*']),
     license="MIT",
     entry_points={
         "console_scripts": [
-            "patho_sam.example_data = patho_sam.util:get_example_wsi_data",
-            "patho_sam.automatic_segmentation = patho_sam.automatic_segmentation:main",
+            "patho_sam.example_data=patho_sam.util:get_example_wsi_data",
+            "patho_sam.automatic_segmentation=patho_sam.automatic_segmentation:main",
         ]
     }
 )
